93bf7d9202ca160fede07f6f2f05396fd18d9dc7
Elod Kuthy <elod.kuthy@hotmail.com>
2014-10-26 10:26:29 +0100

    Canned server skeleton added

---
+++ app/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function () {
+
+    var express    = require('express');
+    var app        = express();
+    var bodyParser = require('body-parser');
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    var port = process.env.PORT || 9000;
+
+    var router = express.Router();
+
+    router.get('/', function(req, res) {
+        res.json({ message: 'hooray! welcome to our api!' });
+    });
+
+    app.use('/api', router);
+
+    app.listen(port);
+    console.log('Server running at port ' + port);
+
+})();
