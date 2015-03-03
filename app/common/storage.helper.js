6372e675aedb9f98b7e31af493b057c15ac6f82a
Elod Kuthy <kuthyelod@gmail.com>
2015-03-03 18:04:52 +0100

    Check Safari private mode at login

--- app/common/storage.helper.js
+++ app/common/storage.helper.js
@@ -1,15 +1,15 @@
 (function (){
 
-    "use strict";
+    'use strict';
 
-    angular.module("gymassistant.front.common")
-        .factory("storageHelper", StorageHelper);
+    angular.module('gymassistant.front.common')
+        .factory('storageHelper', StorageHelper);
 
     /* @ngInject */
-    function StorageHelper($window, ipCookie) {
+    function StorageHelper($window, $location, ipCookie, errorService) {
 
-        var authorizationCookieKey = "authorization";
-        var userInfoCookieKey = "userInfo";
+        var authorizationCookieKey = 'authorization';
+        var userInfoCookieKey = 'userInfo';
 
         return {
             getAuth: getAuth,
@@ -18,53 +18,77 @@
             setUserInfo: setUserInfo
         };
 
-        function getAuth() {
-            var authorization = $window.sessionStorage.authorization;
-            if (!authorization) {
-                authorization = ipCookie(authorizationCookieKey);
-                if (authorization) {
-                    $window.sessionStorage.authorization = authorization;
-                }
+        function checkPrivateMode(err) {
+            if (err.code === 22) {
+                errorService.modal('A Safari privát módban van, így nem tudsz belépni. Válts normál módra, vagy probálkozz más böngészővel!');
+                $location.path('/');
+            } else {
+                throw err;
             }
+        }
 
-            return authorization;
+        function getAuth() {
+            try {
+                var authorization = $window.sessionStorage.authorization;
+                if (!authorization) {
+                    authorization = ipCookie(authorizationCookieKey);
+                    if (authorization) {
+                        $window.sessionStorage.authorization = authorization;
+                    }
+                }
+
+                return authorization;
+            } catch (err) {
+                checkPrivateMode(err);
+            }
         }
 
         function setAuth(authorization, remember) {
-
-            if (authorization) {
-                $window.sessionStorage.authorization = authorization;
-                if (remember) {
-                    ipCookie(authorizationCookieKey, authorization, { expires: 1000 });
+            try {
+                if (authorization) {
+                    $window.sessionStorage.authorization = authorization;
+                    if (remember) {
+                        ipCookie(authorizationCookieKey, authorization, { expires: 1000 });
+                    }
+                } else {
+                    $window.sessionStorage.removeItem('authorization');
+                    ipCookie.remove(authorizationCookieKey);
                 }
-            } else {
-                $window.sessionStorage.removeItem('authorization');
-                ipCookie.remove(authorizationCookieKey);
+            } catch (err) {
+                checkPrivateMode(err);
             }
         }
 
         function getUserInfo() {
-            var userInfo = $window.sessionStorage.userInfo;
-            if (!userInfo) {
-                userInfo = ipCookie(userInfoCookieKey);
-                if (userInfo) {
-                    userInfo = JSON.stringify(userInfo);
-                    $window.sessionStorage.userInfo = userInfo;
+            try {
+                var userInfo = $window.sessionStorage.userInfo;
+                if (!userInfo) {
+                    userInfo = ipCookie(userInfoCookieKey);
+                    if (userInfo) {
+                        userInfo = JSON.stringify(userInfo);
+                        $window.sessionStorage.userInfo = userInfo;
+                    }
                 }
-            }
 
-            return userInfo ? JSON.parse(userInfo) : null;
+                return userInfo ? JSON.parse(userInfo) : null;
+            } catch (err) {
+                checkPrivateMode(err);
+            }
         }
 
         function setUserInfo(userInfo, remember) {
-            if (userInfo) {
-                $window.sessionStorage.userInfo = JSON.stringify(userInfo);
-                if (remember) {
-                    ipCookie(userInfoCookieKey, JSON.stringify(userInfo), { expires: 1000 });
+            try {
+                if (userInfo) {
+                    $window.sessionStorage.userInfo = JSON.stringify(userInfo);
+                    if (remember) {
+                        ipCookie(userInfoCookieKey, JSON.stringify(userInfo), { expires: 1000 });
+                    }
+                } else {
+                    $window.sessionStorage.removeItem('userInfo');
+                    ipCookie.remove(userInfoCookieKey);
                 }
-            } else {
-                $window.sessionStorage.removeItem('userInfo');
-                ipCookie.remove(userInfoCookieKey);
+            } catch (err) {
+                checkPrivateMode(err);
             }
         }
     }
