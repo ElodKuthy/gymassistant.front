9dab1b37556fbfe6927f1c0fa55e272d354e3796
ElodKuthy <kuthyelod@gmail.com>
2015-06-22 16:21:54 +0200

    Update gulp script to copy all images

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,25 @@ var plugins = require("gulp-load-plugins")();
 var config = require('./config.json');
 
 gulp.task("bower_components", function () {
-  return gulp.src("bower_components")
-      .pipe(plugins.webserver({
-        port: 8002
-      }));
+    return gulp.src("bower_components")
+        .pipe(plugins.webserver({
+            port: 8002
+        }));
 });
 
 gulp.task("run:app", ["bower_components"], function () {
     return gulp.src("app")
         .pipe(plugins.webserver({
             fallback: "index.html",
-            https:  {
+            https: {
                 key: "ssl/key.pem",
                 cert: "ssl/cert.pem"
             },
             port: 8001,
-            proxies: [
-                { source: "/bower_components", target: "http://localhost:8002/" }
-            ]
+            proxies: [{
+                source: "/bower_components",
+                target: "http://localhost:8002/"
+            }]
         }));
 });
 
@@ -31,7 +32,7 @@ gulp.task("run:dist", function () {
     return gulp.src("dist")
         .pipe(plugins.webserver({
             fallback: "index.html",
-            https:  {
+            https: {
                 key: "ssl/key.pem",
                 cert: "ssl/cert.pem"
             },
@@ -39,25 +40,28 @@ gulp.task("run:dist", function () {
         }));
 });
 
-gulp.task("clean:dist", function(cb) {
+gulp.task("clean:dist", function (cb) {
     return del("dist", cb);
 });
 
-gulp.task("copy:static", ["clean:dist"], function() {
-    return gulp.src(["app/apple-touch-icon.png", "app/favicon.ico", "app/logo_slice.png"], { base: "app"})
+gulp.task("copy:static", ["clean:dist"], function () {
+    return gulp.src(["app/*.png", "app/favicon.ico"], {
+            base: "app"
+        })
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("copy:fonts", ["clean:dist"], function() {
+gulp.task("copy:fonts", ["clean:dist"], function () {
     return gulp.src("bower_components/fontawesome/fonts/*.*")
         .pipe(gulp.dest("dist/fonts"));
 });
 
 gulp.task("css:vendor", ["clean:dist"], function () {
     return gulp.src([
-        "bower_components/bootstrap/dist/css/bootstrap.min.css",
-        "bower_components/bootstrap/dist/css/bootstrap-theme.min.css",
-        "bower_components/fontawesome/css/font-awesome.min.css"])
+            "bower_components/bootstrap/dist/css/bootstrap.min.css",
+            "bower_components/bootstrap/dist/css/bootstrap-theme.min.css",
+            "bower_components/fontawesome/css/font-awesome.min.css"
+        ])
         .pipe(plugins.concat("vendor.min.css"))
         .pipe(gulp.dest("dist/css"));
 });
@@ -76,22 +80,23 @@ gulp.task("js:modernizr", ["clean:dist"], function () {
 
 gulp.task("js:vendor", ["clean:dist"], function () {
     return gulp.src([
-        "bower_components/jquery/dist/jquery.min.js",
-        "bower_components/bootstrap/dist/js/bootstrap.min.js",
-        "bower_components/angular/angular.min.js",
-        "bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.min.js",
-        "bower_components/angular-animate/angular-animate.min.js",
-        "bower_components/angular-i18n/angular-locale_hu-hu.js",
-        "bower_components/angular-route/angular-route.min.js",
-        "bower_components/angular-resource/angular-resource.min.js",
-        "bower_components/moment/min/moment.min.js",
-        "bower_components/angular-moment/angular-moment.min.js",
-        "bower_components/angular-cookie/angular-cookie.min.js",
-        "bower_components/qcode-decoder/build/qrcode.js",
-        "bower_components/qrcode/lib/qrcode.min.js",
-        "bower_components/angular-qr/angular-qr.min.js",
-        "bower_components/angular-sanitize/angular-sanitize.min.js",
-        "bower_components/ng-csv/build/ng-csv.min.js"])
+            "bower_components/jquery/dist/jquery.min.js",
+            "bower_components/bootstrap/dist/js/bootstrap.min.js",
+            "bower_components/angular/angular.min.js",
+            "bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.min.js",
+            "bower_components/angular-animate/angular-animate.min.js",
+            "bower_components/angular-i18n/angular-locale_hu-hu.js",
+            "bower_components/angular-route/angular-route.min.js",
+            "bower_components/angular-resource/angular-resource.min.js",
+            "bower_components/moment/min/moment.min.js",
+            "bower_components/angular-moment/angular-moment.min.js",
+            "bower_components/angular-cookie/angular-cookie.min.js",
+            "bower_components/qcode-decoder/build/qrcode.js",
+            "bower_components/qrcode/lib/qrcode.min.js",
+            "bower_components/angular-qr/angular-qr.min.js",
+            "bower_components/angular-sanitize/angular-sanitize.min.js",
+            "bower_components/ng-csv/build/ng-csv.min.js"
+        ])
         .pipe(plugins.concat("vendor.min.js"))
         .pipe(gulp.dest("dist/js"));
 });
@@ -117,18 +122,24 @@ gulp.task("html_replace", ["clean:dist"], function () {
 });
 
 gulp.task("deploy:public", ["build"], function () {
-    return gulp.src(["dist/*.*", "dist/css/*", "dist/fonts/*", "!dist/*.html"], { base: "dist"})
+    return gulp.src(["dist/*.*", "dist/css/*", "dist/fonts/*", "!dist/*.html"], {
+            base: "dist"
+        })
         .pipe(gulp.dest(config.deploy.target + "/public"));
 });
 
 gulp.task("deploy:js", ["build"], function () {
-    return gulp.src(["dist/js/*"], { base: "dist"})
+    return gulp.src(["dist/js/*"], {
+            base: "dist"
+        })
         .pipe(plugins.replace(/https:\/\/localhost:8000/g, ""))
         .pipe(gulp.dest(config.deploy.target + "/public"));
 });
 
 gulp.task("deploy:views", ["build"], function () {
-    return gulp.src(["dist/**/*.html"], { base: "dist"})
+    return gulp.src(["dist/**/*.html"], {
+            base: "dist"
+        })
         .pipe(gulp.dest(config.deploy.target + "/views"));
 });
 
